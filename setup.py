ebf881f58ebd36c14449bd838ec8917d0a9c0462
aliavictor <alia.jo.victor@gmail.com>
2023-03-24 14:37:11 -0400

    Incremented package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alia",
-    version="0.1.2",
+    version="0.1.3",
     description="A collection of random helper tools to make life easier",
     author="Alia",
     author_email="alia.jo.victor@gmail.com",
