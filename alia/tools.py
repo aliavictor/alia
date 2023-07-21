ab003a296b6167e8fc47eef63a8ea76ce99b3ddf
aliavictor <alia.jo.victor@gmail.com>
2023-07-21 12:54:53 -0400

    Fixed bug with filelist function

--- alia/tools.py
+++ alia/tools.py
@@ -911,7 +911,7 @@ def filelist(dirpath, ext=None, prefix=None, raise_err=False):
         elif prefix:
             return [i for i in os.listdir(dirpath) if i.startswith(prefix)]
         else:
-            return [i for i in os.listdir(dirpath) if i[0].isalnum()]
+            return [i for i in os.listdir(dirpath)]
     except FileNotFoundError:
         if raise_err:
             raise FileNotFoundError
