8a106196968c23e63abe082195811ebaae36074c
aliavictor <alia.jo.victor@gmail.com>
2023-07-24 12:43:02 -0400

    Reverted function sorting since it was breaking stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alia",
-    version="0.2.7",
+    version="0.2.8",
     description="A collection of random helper tools to make life easier",
     author="Alia",
     author_email="alia.jo.victor@gmail.com",
